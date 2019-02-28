04478741df78d332b7b65e31fa66e6df12c13276
Nicolei Ocana <nicoleiocana@gmail.com>
2019-02-28 18:09:54 +0000

    finish implementation of post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,52 @@
 class PostsController < ApplicationController
-    def index
-    
-    end
-    
-    def new
-    
-    end
+  def index
+    @posts = Post.order(created_at: :desc)
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
 
-    def create
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.save
+      redirect_to @post
         
+    else
+      render 'new'
     end
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    
+    @post.destroy
+    
+    redirect_to root_path
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :content) 
+  end
 end
